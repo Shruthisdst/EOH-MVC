6f8a96379c86340216c7c86af52f48be04560e3e
Shruthisdst <shruthitr.nayak@gmail.com>
2016-07-13 17:41:50 +0530

    No. 4 - footer contents removed

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,15 +3,19 @@
         <div class="container"><!-- container -->
             <div class="row"><!-- row -->
                 <div class="col-lg-6">
+<!--
                     <p class="copy-text text-left">
                         © 2016 University of Mysore.
                     </p>
+-->
                 </div>
                 <div class="col-lg-6">
                     <p class="copy-text text-right">
                         <!-- <a href="">Terms of Use</a> | <a href="">Privacy Policy</a> | <a href="">Sitemap</a> -->
+<!--
                         <a href="<?=BASE_URL?>About_IASc/Contact_details/">Contact</a> | 
                         <a href="<?=BASE_URL?>About_IASc/Site_Index">Site index</a>
+-->
                     </p>
                 </div>
             </div><!-- row end -->
