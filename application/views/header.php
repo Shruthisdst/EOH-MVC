f51e27ea0345b467590cfd9e69f07b245d125650
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-12-29 20:52:48 +0530

    versioning and cleanup

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
     <!-- Basic Page Needs
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
-    <title><?php if($pageTitle) echo $pageTitle . ' | '; ?>Encyclopaedia of Hinduism </title>
+    <title><?php if($pageTitle) echo $pageTitle . ' | '; ?>A Concise Encyclopaedia of Hinduism </title>
     <meta name="description" content="">
     <meta name="author" content="">
 
@@ -23,18 +23,10 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/viewer.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/navbar.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/homepage.css">
-    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/dict.css">
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/aux.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/carousel.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/page.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/archive.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/flat.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/form.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/social.css"> -->
-    <!-- <link rel="stylesheet" href="<?=PUBLIC_URL?>css/general.css"> -->
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/viewer.css?v=1.0">
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/navbar.css?v=1.0">
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/homepage.css?v=1.0">
+    <link rel="stylesheet" href="<?=PUBLIC_URL?>css/dict.css?v=1.0">
 
     <!-- Fonts
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -49,13 +41,13 @@
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <nav id="mainNavBar" class="navbar navbar-light navbar-expand-lg fixed-top">
         <div class="container-fluid clear-paddings">
-            <a class="navbar-brand" href="#"><img src="<?=PUBLIC_URL?>images/logo.png" alt="Logo" class="logo"></a>
+            <a class="navbar-brand" href="<?=BASE_URL?>"><img src="<?=PUBLIC_URL?>images/logo.png" alt="Logo" class="logo"></a>
             <p class="navbar-text" id="navbarText"><small>A Concise</small><br />Encyclopaedia of Hinduism</p>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav nav ml-auto">
-                    <li><a href="<?=BASE_URL?>page/flat/Home">The Encyclopaedia</a></li>
-                    <li><a href="<?=BASE_URL?>page/flat/About">About</a></li>
+                    <li><a href="<?=BASE_URL?>">The Encyclopaedia</a></li>
+                    <li><a href="<?=BASE_URL?>About">About</a></li>
                     <li><a href="<?=BASE_URL?>listing/alphabet/A">Index</a></li>
                     <li id="openNavbarSearch"><a><i class="fa fa-search"></i></a></li>
                 </ul>
